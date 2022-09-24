f2bd78eaaa0832621158a6755b15cc9eb5a9af29
Bayan Omar <Bayan.alhumaidan@gmail.com>
2022-09-24 15:35:48 +0300

    index.html out of public

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const https = require("https");
-const serverless = require('serverless-http');
+//const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -22,4 +22,4 @@ app.use(function (req, res, next) {
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 })
-module.exports.handler = serverless(app);
+//module.exports.handler = serverless(app);
