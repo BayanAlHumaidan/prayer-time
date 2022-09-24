3c391cd15390929ecf0c3faa4b70b7e14fd51b1e
Bayan Omar <Bayan.alhumaidan@gmail.com>
2022-09-24 15:28:16 +0300

    Third Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+
+app.listen(process.env.PORT | 3000);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
